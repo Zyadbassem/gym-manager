ed0840d9b2ff5b277fbc7aa06e96495797c8e6d3
zyad <zyadbassem9090@gmail.com>
2026-04-04 17:19:37 +0200

    fix the index file of the db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,20 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import { admins as adminTable } from "./schema/users.js";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
+
+function testConnection() {
+  db.select()
+    .from(adminTable)
+    .then((result) => {
+      console.log("Database connection successful:", result);
+    })
+    .catch((error) => {
+      console.error("Database connection failed:", error);
+    });
+}
+
+testConnection();
